f9e3843f77c47f3a9f31cf7d2f7d8c2dd996dc72
sup1p <150022089+sup1p@users.noreply.github.com>
2024-11-18 21:56:06 +0500

    Add files via upload

---
+++ demostud/src/main/java/com/studybuddy/demostud/DemostudApplication.java
@@ -0,0 +1,15 @@
+package com.studybuddy.demostud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+
+public class DemostudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemostudApplication.class, args);
+	}
+}
